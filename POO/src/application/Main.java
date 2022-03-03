1847f4623eba18acb72f7636d06a85cdb711307f
Silasoli <silas.oliveira@blockhub.builders>
2022-03-03 16:39:24 -0300

    aplicando conceito de encapsulamento

--- POO/src/application/Main.java
+++ POO/src/application/Main.java
@@ -6,22 +6,22 @@ import model.Vehicle;
  *
  * @author silas
  */
-public class Main{
+public class Main {
 
     public static void main(String[] args) {
-    
+
         Vehicle vehicle1 = new Vehicle();
+
+        vehicle1.setName("Lancer 2.0 Evolution X 4x4 16v Turbo");
+        vehicle1.setBrand("Mitsubishi");
+        vehicle1.setChassi("753 AAlYEr 0g 8y9730");
+        vehicle1.setQtywheels(4);
         
-        vehicle1.name = "Lancer 2.0 Evolution X 4x4 16v Turbo";
-        vehicle1.brand = "Mitsubishi";
-        vehicle1.chassi = "753 AAlYEr 0g 8y9730";
-        vehicle1.qtywheels = 4;
-        
+        vehicle1.supplyFuel(10);
+
         System.out.println(vehicle1);
         vehicle1.turnOn();
 
-        
     }
-    
 
 }
--- POO/src/model/Vehicle.java
+++ POO/src/model/Vehicle.java
@@ -5,22 +5,66 @@ package model;
  * @author silas
  */
 public class Vehicle {
+
+    private String name;
+    private String brand;
+    private String chassi;
+    private int qtywheels;
+    private float qtyFuel = 0;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getChassi() {
+        return chassi;
+    }
+
+    public void setChassi(String chassi) {
+        this.chassi = chassi;
+    }
+
+    public int getQtywheels() {
+        return qtywheels;
+    }
+
+    public void setQtywheels(int qtywheels) {
+        this.qtywheels = qtywheels;
+    }
+
+    public float getQtyFuel() {
+        return qtyFuel;
+    }
+
+    public void supplyFuel(float liters){
+        qtyFuel += liters;
+        
+    }
     
-    public String name;
-    public String brand;
-    public String chassi;
-    public int qtywheels;
     
-    public void turnOn(){
+    
+    public void turnOn() {
         System.out.println("Ligou o veículo!");
     }
-    
-     public void turnOff(){
+
+    public void turnOff() {
         System.out.println("Desligou o veículo!");
     }
-     
+
     @Override
-    public String toString(){
-        return "Nome: "+name+"\n"+"Marca: "+brand+"\n"+"Chassi: "+chassi+"\n"+"Quantidade de rodas: "+qtywheels;
+    public String toString() {
+        return "Nome: " + name + "\n" + "Marca: " + brand + "\n" + "Chassi: " + chassi + "\n" + "Quantidade de rodas: " + qtywheels+"\n"+ "Quantidade de gasolina: "+qtyFuel;
     }
 }
