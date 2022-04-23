b4797711077fc1a05b67b77f4e457ca8c4e4e7fb
Silasoli <silas.oliveira@blockhub.builders>
2022-04-23 12:38:55 -0300

    Sobrecarga de métodos

--- POO/src/application/Main.java
+++ POO/src/application/Main.java
@@ -13,16 +13,20 @@ public class Main {
 
         try {
             Car vehicle1 = new Car();
-
-            vehicle1.setName("Lancer 2.0 Evolution X 4x4 16v Turbo");
-            vehicle1.setBrand("Mitsubishi");
-            vehicle1.setChassi("753AAlYEr0g8y9730");
+            //vehicle1.setName("Lancer 2.0 Evolution X 4x4 16v Turbo");
+            //vehicle1.setBrand("Mitsubishi");
+            //vehicle1.setChassi("753AAlYEr0g8y9730");
             //vehicle1.setQtywheels(4);
 
-            vehicle1.supplyFuel(10);
+            //vehicle1.supplyFuel(10);
+            //System.out.println(vehicle1);
+            //vehicle1.turnOn();
+            Car vehicle2 = new Car("Lancer EVO", "Mitsubishi");
+            System.out.println(vehicle2.getName());
 
-            System.out.println(vehicle1);
-            vehicle1.turnOn();
+            Car vehicle3 = new Car("Lancer 2012", "Mitsubishi", "753AAlYEr0g8y9729");
+            System.out.println(vehicle3.getName());
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
 
--- POO/src/model/Car.java
+++ POO/src/model/Car.java
@@ -4,21 +4,36 @@
  */
 package model;
 
+import exceptions.ChassiExceptions;
+
 /**
  *
  * @author silas
  */
 public class Car extends Vehicle {
 
-    public Car(){
+    public Car() {
         this.qtywheels = 4;
+    }
+    //public Car(String name, float value) {}
+
+    //public Car(float km, String name) {}
+    public Car(String name, String brand) {
+        this.qtywheels = 4;
+        this.setName(name);
+        this.setBrand(brand);
+    }
+
+    public Car(String name, String brand, String chassi) throws ChassiExceptions {
+        this.qtywheels = 4;
+        this.setName(name);
+        this.setBrand(brand);
+        this.setChassi(chassi);
     }
 
     @Override
-   public String toString() {
-        return "Nome: " + getName() + "\n" + "Marca: " + getBrand() + "\n" + "Chassi: " + getChassi() + "\n" + "Quantidade de rodas: " + getQtywheels() +"\n"+ "Quantidade de gasolina: "+getQtyFuel();
+    public String toString() {
+        return "Nome: " + getName() + "\n" + "Marca: " + getBrand() + "\n" + "Chassi: " + getChassi() + "\n" + "Quantidade de rodas: " + getQtywheels() + "\n" + "Quantidade de gasolina: " + getQtyFuel();
     }
-    
-    
-    
+
 }
